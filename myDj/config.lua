ae3f550b2b5093fe3005ed32f2e2fe06b698c9df
Strips_ <99659982+StripsDev@users.noreply.github.com>
2022-02-17 20:27:47 +0100

    Update

---
+++ myDj/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+Translation = {}
+
+Translation = {
+    ['de'] = {
+        ['DJ_interact'] = 'Drücke ~g~E~s~, um auf das DJ Pult zuzugreifen',
+        ['title_does_not_exist'] = '~r~Dieser Titel existiert nicht!',
+    },
+
+    ['en'] = {
+        ['DJ_interact'] = 'Press ~g~E~s~, to access the DJ desk',
+        ['title_does_not_exist'] = '~r~This title does not exist!',
+    }
+}
+
+Config.Locale = 'en'
+
+Config.useESX = true -- can not be disabled without changing the callbacks
+Config.enableCommand = false
+
+Config.enableMarker = true -- purple marker at the DJ stations
+
+Config.DJPositions = {
+    {name = 'bliss', pos = vector3(4893.65, -4905.47, 3.49), requiredJob = 'bliss', range = 42.0, volume = 0.5}
+}
